22845c57a12236d160b403afe651a707b9128e7e
Matt Sanders <matt.d.s.sanders@gmail.com>
2016-08-12 16:41:23 +1200

    added scss to the build process

--- src/App.vue
+++ src/App.vue
@@ -32,33 +32,11 @@ export default {
 }
 </script>
 
-<style>
-html {
-  height: 100%;
-}
+<style lang="scss">
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
+ $icon-font-path: "../node_modules/bootstrap-sass/assets/fonts/bootstrap/";
+ $brand-primary: #213b52;
+@import '../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap'
 
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+ 
 </style>
