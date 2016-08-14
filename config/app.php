74182dcddd2320d75ab8f9bb373bd6e045a761f8
Matt Sanders <matt.d.s.sanders@gmail.com>
2016-08-15 10:07:04 +1200

    Added JWT Auth

--- config/app.php
+++ config/app.php
@@ -198,6 +198,7 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
