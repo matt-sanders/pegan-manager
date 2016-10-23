9e149042401c5dfedd187b61dc8aed41ea26350d
Matt Sanders <matt.d.s.sanders@gmail.com>
2016-10-24 09:44:55 +1300

    first build

--- resources/assets/js/constants.js
+++ resources/assets/js/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api/';
-console.log(window);
+import environments from 'gulp-environments';
+export const API_URL = environments.production() ? 'https://pegan-recipe-manager.herokuapp.com' : 'http://localhost:8000/api/';
