f27ed18f442d2d046e3ad7ed57b7c563f1fa74a5
Matt Sanders <matt.d.s.sanders@gmail.com>
2016-10-24 16:51:09 +1300

    added db seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Matt Sanders',
+            'email' => 'matt@twitchdevelopment.com',
+            'password' => bcrypt('test')
+        ]);
     }
 }
