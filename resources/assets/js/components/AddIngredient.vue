693a205b35188fda5b00bd75ba0b955c588ba9a6
Matt Sanders <matt.d.s.sanders@gmail.com>
2016-11-11 11:34:54 +1300

    fixed sorting issue with case sensitivity

--- resources/assets/js/components/AddIngredient.vue
+++ resources/assets/js/components/AddIngredient.vue
@@ -60,8 +60,8 @@
              availableIngredients: state => {
                  let ings = [...state.ingredients.ingredients];
                  return ings.sort(function(a, b){
-                     if ( a.title < b.title ) return -1;
-                     if ( a.title > b.title ) return 1;
+                     if ( a.title.toLowerCase() < b.title.toLowerCase() ) return -1;
+                     if ( a.title.toLowerCase() > b.title.toLowerCase() ) return 1;
                      return 0;
                  });
              },
